a8609d12892058ec18fd9536c6d3c3b1d4e1005d
Helen <heb264@gmail.com>
2017-12-12 10:35:45 +0800

    finished working on project so far with basics.

---
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "a test recipe",
+      "this is a test",
+      "https://picsum.photos/200/300"
+    ),
+    new Recipe(
+      "a test recipe",
+      "this is a test",
+      "https://picsum.photos/200/300"
+    ),
+    new Recipe(
+      "a test recipe",
+      "this is a test",
+      "https://picsum.photos/200/300"
+    )
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
