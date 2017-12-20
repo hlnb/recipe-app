26ae00f891452e71e25180f8c94207d79e980a95
Helen <heb264@gmail.com>
2017-12-20 10:42:25 +0800

    added dropdown directive

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive'; 
 
 
 
 @NgModule({
-  declarations: [AppComponent, HeadrComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
+  declarations: [AppComponent, HeadrComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent,
+  DropdownDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
